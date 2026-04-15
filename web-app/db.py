07a300bd71510c90e2ae7ef962d7d76c5c93d680
ValeriaChang <valeriachang06@gmail.com>
2026-04-15 11:10:32 -0400

    db fixes

--- web-app/db.py
+++ web-app/db.py
@@ -3,7 +3,7 @@ import os
 from pymongo import MongoClient
 
 client = MongoClient(os.getenv("MONGO_URI", "mongodb://localhost:27017/"))
-db = client["speech_rater"]
+db = client["sillysealions"]
 
 users_collection = db["users"]
 speeches_collection = db["speeches"]
